1af35190bac7bb1f9e9d7d9fd00ac4abb0ced3dc
Dan Groze <dangroze@gmail.com>
2018-12-20 12:38:01 +0000

    Step8 ex8

--- directory.rb
+++ directory.rb
@@ -44,15 +44,27 @@ def print_header
   puts "--------------------------------"
 end
 
+def group_cohort(students)
+  groups = {}
+  students.each do |student|
+    cohort = student[:cohort].to_sym
+    if groups[cohort] == nil
+      groups[cohort] = []
+    end
+    groups[cohort].push(student)
+  end
+  groups
+end
+
 def print(students)
-  tally = 0
-  while tally < students.count
-    puts "#{tally + 1}. #{students[tally][:name]}".center(32, "-")
-    puts "-nationality: #{students[tally][:nationality]}".center(37) 
-    puts "-age: #{students[tally][:age]}".center(37)
-    puts "-height: #{students[tally][:height]}".center(37)
-    puts "-cohort: #{students[tally][:cohort]}".center(37)
-    tally += 1
+  students.each do |cohort, cohort_students|
+    puts "#{cohort.capitalize} Cohort".center(32, "-")
+    cohort_students.each_with_index do |identity , index|
+      puts "#{index + 1}. #{identity[:name]}".center(32, "-")
+      puts "-nationality: #{identity[:nationality]}".center(37) 
+      puts "-age: #{identity[:age]}".center(37)
+      puts "-height: #{identity[:height]}".center(37)
+    end
   end
 end
 
@@ -62,6 +74,7 @@ end
 # nothing happens unless we call the methods
 students = input_students
 length = print_students(students)
+grouped_students = group_cohort(length)
 print_header
-print(length)
+print(grouped_students)
 print_footer(students)
