b96572abdbce9f0574b9fcf5a35bd09682fc8a5e
Dan Groze <dangroze@gmail.com>
2018-12-21 13:29:52 +0000

    Step 10 -refactoring

--- directory.rb
+++ directory.rb
@@ -1,13 +1,9 @@
-# let's puts students into an array
+@students = []
 
 def input_students
   puts "Please enter the names of students"
-  puts "To finish, just hit return twice"
-  #create an empty array
-  students = []
-  #get the first name
-  name = gets.rstrip
-  #while name is not empty, repeat this line
+  puts "To finish, hit return twice"
+  name = gets.chomp
   while !name.empty? do
     puts "Which cohort is the student in?"
     cohort = gets.chomp
@@ -16,27 +12,18 @@ def input_students
     else
       cohort = cohort.to_sym
     end
-    puts "What is the student\'s nationality?"
+    puts "What is the student's nationality?"
     nationality = gets.chomp
-    puts "What is the student\'s age?"
+    puts "What is the student's age?"
     age = gets.chomp
-    puts "What is the student\'s height?"
+    puts "What is the student's height?"
     height = gets.chomp
-    
-    #add the students hash to the array
-    students << {name: name, cohort: cohort, nationality: nationality, 
+    @students << {name: name, cohort: cohort, nationality: nationality, 
     age: age, height: height}
-    puts "Now we have #{students.count} students"
-    #get another name from the user
+    puts "Now we have #{@students.count} students"
     puts"Please enter another name, or press enter to skip"
     name = gets.chomp
   end
-  #return the array of students
-  students
-end
-
-def print_students(students)
-  students.select {|student| student[:name].length <= 12}
 end
 
 def print_header
@@ -56,7 +43,7 @@ def group_cohort(students)
   groups
 end
 
-def print(students)
+def print_students_list(students)
   students.each do |cohort, cohort_students|
     puts "#{cohort.capitalize} Cohort".center(32, "-")
     cohort_students.each_with_index do |identity , index|
@@ -76,39 +63,39 @@ def print_footer(students)
   end
 end  
 
+def print_menu
+  #1. Print the menu and ask user what to do
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit" # 9 because we'll be adding more options
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
+  end
+end
+
+def show_students
+  grouped_students = group_cohort(@students)
+  print_header
+  print_students_list(grouped_students)
+  print_footer(@students)
+end
+
 def interactive_menu
-  students = []
   loop do
-    #1. Print the menu and ask user what to do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit" # 9 because we'll be adding more options
-    #2. Read the input and save it into a variable
-    selection = gets.chomp
-    #3. Do what the user has asked
-    case selection
-      when "1"
-        students = input_students
-      when "2"
-        length = print_students(students)
-        grouped_students = group_cohort(length)
-        print_header
-        print(grouped_students)
-        print_footer(students)
-      when "9"
-        exit # This will cause the program to terminate
-      else
-        puts "I don't know what you mean, try again"
-    end
+    print_menu
+    process(gets.chomp)
   end
 end
 
 interactive_menu
   
-# nothing happens unless we call the methods
-#students = input_students
-#length = print_students(students)
-#grouped_students = group_cohort(length)
-#print_header
-#print(grouped_students)
-#print_footer(students)
