da47bb6625d295d6fef016107c8fc354e09b752b
Dan Groze <dangroze@gmail.com>
2018-12-30 12:08:19 +0000

    Step13

--- directory.rb
+++ directory.rb
@@ -3,21 +3,21 @@
 def input_students
   puts "Please enter the names of students"
   puts "To finish, hit return twice"
-  name = gets.chomp
+  name = STDIN.gets.chomp
   while !name.empty? do
     puts "Which cohort is the student in?"
-    cohort = gets.chomp
+    cohort = STDIN.gets.chomp
     if cohort == ""
       cohort = "Not assigned"
     else
       cohort = cohort.to_sym
     end
     puts "What is the student's nationality?"
-    nationality = gets.chomp
+    nationality = STDIN.gets.chomp
     puts "What is the student's age?"
-    age = gets.chomp
+    age = STDIN.gets.chomp
     puts "What is the student's height?"
-    height = gets.chomp
+    height = STDIN.gets.chomp
     @students << {name: name, cohort: cohort, nationality: nationality, 
     age: age, height: height}
     puts "Now we have #{@students.count} students"
@@ -115,13 +115,25 @@ def save_students
   file.close
 end
 
-def load_students
-  file = File.open("students.csv", "r")
+def load_students(filename = "students.csv")
+  file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(",")
     @students << {name: name, cohort: cohort.to_sym}
   end
   file.close
+end
+
+def try_load_students
+  filename = ARGV.first # first argument from the command line
+  return if filename.nil? # get out of the method if it isn't given
+  if File.exists?(filename) # if it exists
+    load_students(filename)
+    puts "Loaded #{@students.count} from #{filename}"
+  else # if it doesn't exist
+    puts "Sorry, #{filename} doesn't exist"
+    exit # quit the program
+  end
 end
 
 interactive_menu
